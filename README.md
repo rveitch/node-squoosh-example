9cc3dcfdc48b747fa7cedddd3c9bd88093d2e8c5
ryan.veitch <ryan.r.veitch@gmail.com>
2021-06-10 15:12:51 -0500

    [CHORE] Add exif parsing for before/after comparison info

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ See more at https://github.com/GoogleChromeLabs/squoosh/tree/dev/libsquoosh
 - `$ git clone https://github.com/rveitch/node-squoosh-example.git`
 - `$ npm install`
 - Run `$ npm start` to initialize the app.
+
+## Output
+This example will log each step and give a before/after comparison similar to:
+```
+Image Decoding Complete
+Image Pre-Processing Complete
+Image Encoding Complete
+Image Conversion Complete
+Image Pool Closed
+convertImage: 4.085s
+
+Before - File Size: 2025 KiB, Image Size: 3024x4032
+After - File Size: 167 KiB, Image Size: 1080x1440
+```
--- image-converter.js
+++ image-converter.js
@@ -1,18 +1,20 @@
 // const { ImagePool } = require('@squoosh/lib'); // Doesn't work due to bug in package.json (https://github.com/GoogleChromeLabs/squoosh/issues/1034)
 const { ImagePool } = require('@squoosh/lib/build/index.js'); // Work around
+const ExifTool = require("exiftool-vendored").exiftool
 const fs = require('fs');
 
-const RAW_IMAGES_FOLDER = './raw-images/'
-const CONVERTED_IMAGES_FOLDER = './converted-images/'
+const RAW_IMAGES_FOLDER = `${__dirname}/raw-images/`;
+const CONVERTED_IMAGES_FOLDER = `${__dirname}/converted-images/`;
 
 async function convertImage(inputFilname, outputFilname) {
   const inputPath = `${RAW_IMAGES_FOLDER}${inputFilname}`;
   const outputPath = `${CONVERTED_IMAGES_FOLDER}${outputFilname}`;
 
+  console.time('convertImage');
   const imagePool = new ImagePool();
   const image = imagePool.ingestImage(inputPath);
   await image.decoded; //Wait until the image is decoded before running preprocessor
-  console.info('Image Decoding Complete');
+  console.info('\nImage Decoding Complete');
 
   // Pre-process
   const preprocessOptions = {
@@ -41,11 +43,16 @@ async function convertImage(inputFilname, outputFilname) {
 
   await imagePool.close();
   console.info('Image Pool Closed');
+  console.timeEnd('convertImage');
+
+  const { FileSize: FileSizeBefore, ImageSize: ImageSizeBefore } = await ExifTool.read(inputPath);
+  console.log(`\nBefore - File Size: ${FileSizeBefore}, Image Size: ${ImageSizeBefore}`);
+
+  const { FileSize: FileSizeAfter, ImageSize: After } = await ExifTool.read(outputPath);
+  console.log(`After - File Size: ${FileSizeAfter}, Image Size: ${After}\n`);
 }
 
 (async () => {
-  console.time('convertImage');
   await convertImage('norma-teef-1.jpg', 'norma-teef-1-converted.jpg');
-  console.timeEnd('convertImage');
 })();
 
